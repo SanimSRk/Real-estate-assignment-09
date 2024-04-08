b37812f8322877050f75fc740a2c693f105b5b0e
SanimSRk <hasansanim562@gmail.com>
2024-04-08 18:23:41 +0600

    cartd section every ting is done

--- src/Compment/Cart/Cartds.jsx
+++ src/Compment/Cart/Cartds.jsx
@@ -1,9 +1,37 @@
 const Cartds = ({ resnt }) => {
-  const { image, title } = resnt;
+  const { id, image, title, status, segment_name, facilities, price, area } =
+    resnt;
   return (
     <div className="shadow-lg p-4 ">
-      <img src={image} alt="" />
-      <h2>{title}</h2>
+      <div>
+        <div>
+          <img src={image} alt="" />
+        </div>
+        <div className="-mt-12 text-end ">
+          <button className="border-none text-white bg-[#FF9638] rounded-none  btn uppercase  font-bold">
+            {status}
+          </button>
+        </div>
+      </div>
+
+      <h2 className="text-xl mt-3 font-semibold">{segment_name}</h2>
+      <div>
+        <h2 className="text-xl font-semibold mt-3">Facilities</h2>
+        {facilities?.map(facil => (
+          <li key={id}>{facil}</li>
+        ))}
+      </div>
+      <div className="flex justify-between mt-3 items-center">
+        <div className="">
+          <h2 className="font-semibold">Price : {price}</h2>
+        </div>
+        <div>
+          <p className="font-semibold">Area : {area}</p>
+        </div>
+      </div>
+      <button className="btn mt-6 font-semibold text-white bg-[#FF9638]">
+        View Property
+      </button>
     </div>
   );
 };
